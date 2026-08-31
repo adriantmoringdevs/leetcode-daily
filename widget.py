46f335d4dd02704569a02947894158390e7c555d
Adrian Moring <adriantmoringbass@gmail.com>
2026-08-31 15:49:12 -0400

    updated UI to reflect question details

--- widget.py
+++ widget.py
@@ -1,15 +1,17 @@
-import sys
+from datetime import datetime
 
+from PySide6.QtCore import QTimer
 from PySide6.QtWidgets import (
-    QApplication,
     QLabel,
     QVBoxLayout,
-    QWidget
+    QWidget,
 )
 
-from leetcode import solved_today
+from leetcode import todays_submissions
+
 
 class LeetCodeWidget(QWidget):
+
     def __init__(self, username):
         super().__init__()
 
@@ -19,20 +21,47 @@ class LeetCodeWidget(QWidget):
 
         self.setup_ui()
 
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.refresh_status)
+        self.timer.start(10 * 1000)
+
     def setup_ui(self):
         layout = QVBoxLayout()
 
-        title = QLabel("LEETCODE DAILY")
-        status = QLabel(self.get_status())
+        self.title = QLabel("LEETCODE DAILY")
+        self.status = QLabel()
+        self.problem = QLabel()
+        self.time = QLabel()
 
-        layout.addWidget(title)
-        layout.addWidget(status)
-
+        layout.addWidget(self.title)
+        layout.addWidget(self.status)
+        layout.addWidget(self.problem)
+        layout.addWidget(self.time)
 
         self.setLayout(layout)
 
-    def get_status(self):
-        if solved_today(self.username):
-            return "🟢 You solved a problem today!"
+        self.refresh_status()
+
+    def refresh_status(self):
+        print("Checking LeetCode...")
+
+        submissions = todays_submissions(self.username)
+
+        if submissions:
+            latest_submission = submissions[0]
+
+            self.status.setText("🟢 DONE TODAY")
+            self.problem.setText(latest_submission["title"])
+
+            solved_time = datetime.fromtimestamp(
+                int(latest_submission["timestamp"])
+            ).strftime("%I:%M %p")
+
+            self.time.setText(f"Solved at {solved_time}")
+
         else:
-            return "🔴 You haven't solved a problem today."
+            self.status.setText("🔴 NOT DONE")
+            self.problem.setText(
+                "You haven't solved a problem today."
+            )
+            self.time.setText("")
